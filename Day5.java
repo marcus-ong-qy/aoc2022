5f2340b866d79ed22614302a833fda31ebb9dfed
HP <marcus.ong.qy@gmail.com>
2022-12-05 15:37:47 +0800

    day 5 complete

---
+++ Day5.java
@@ -0,0 +1,48 @@
+// Day 5
+// https://adventofcode.com/2022/day/5
+import java.util.*;
+import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Day5 {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+ 
+    String[] crates = {
+        "WRF", "THMCDVWP", "PMZNL", 
+        "JCHR", "CPGHQTB", "GCWLFZ", 
+        "WVLQZJGC", "PNRFWTVC", "JWHGRSV"
+    };
+      
+    String pattern = "^move (\\d+) from (\\d+) to (\\d+)$";
+    Pattern r = Pattern.compile(pattern);
+
+    while(in.hasNext()) {
+      String line = in.nextLine();
+      if (line.equals("q!")) break;
+      Matcher matcher = r.matcher(line);
+
+      if (matcher.find()) {
+        int moveCount = Integer.parseInt(matcher.group(1));
+        int fromStackIdx = Integer.parseInt(matcher.group(2))-1;
+        int toStackIdx = Integer.parseInt(matcher.group(3))-1;
+        
+        for (int i=0; i<moveCount; i++) {
+          String fromCrateString = crates[fromStackIdx];
+          char toMove = fromCrateString.charAt(fromCrateString.length()-1);
+          
+          crates[fromStackIdx] = fromCrateString.substring(0, fromCrateString.length()-1);
+          crates[toStackIdx] = crates[toStackIdx] + toMove;
+        }
+      }  
+    }
+
+    for (int i=0; i<9; i++) {
+      String crateString = crates[i];
+      System.out.print(crateString.charAt(crateString.length()-1));
+    }
+
+    in.close();
+  }
+}
---
+++ Day5_2.java
@@ -0,0 +1,47 @@
+// Day 5 Part 2
+// https://adventofcode.com/2022/day/5#part2
+import java.util.*;
+import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Day5_2 {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+ 
+    String[] crates = {
+        "WRF", "THMCDVWP", "PMZNL", 
+        "JCHR", "CPGHQTB", "GCWLFZ", 
+        "WVLQZJGC", "PNRFWTVC", "JWHGRSV"
+    };
+      
+    String pattern = "^move (\\d+) from (\\d+) to (\\d+)$";
+    Pattern r = Pattern.compile(pattern);
+
+    while(in.hasNext()) {
+      String line = in.nextLine();
+      if (line.equals("q!")) break;
+      Matcher matcher = r.matcher(line);
+
+      if (matcher.find()) {
+        int moveCount = Integer.parseInt(matcher.group(1));
+        int fromStackIdx = Integer.parseInt(matcher.group(2)) - 1;
+        int toStackIdx = Integer.parseInt(matcher.group(3)) - 1;
+        
+        String fromCrateString = crates[fromStackIdx];
+        int fromCratesLength = fromCrateString.length();
+        String cratesToMove = fromCrateString.substring(fromCratesLength-moveCount, fromCratesLength);
+        
+        crates[fromStackIdx] = fromCrateString.substring(0,fromCratesLength-moveCount);
+        crates[toStackIdx] = crates[toStackIdx] + cratesToMove;
+      }   
+    }
+
+    for (int i=0; i<9; i++) {
+      String crateString = crates[i];
+      System.out.print(crateString.charAt(crateString.length()-1));
+    }
+
+    in.close();
+  }
+}
