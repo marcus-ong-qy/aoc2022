2969c80941555ccba04d0e4a950744ea9ea5ec1b
HP <marcus.ong.qy@gmail.com>
2022-12-06 14:16:20 +0800

    day 6 complete - improved 6.2

--- Day6_2.java
+++ Day6_2.java
@@ -4,30 +4,34 @@ import java.util.*;
 import java.io.*;
 
 public class Day6_2 {
-  static boolean checkMarker(String substr, int markerLength) {
-    for (int i=0; i<markerLength-1; i++) {
-      for (int j=i+1; j<markerLength; j++) {
-        if (substr.charAt(i) == substr.charAt(j)) {
-          return false;
-        }
+  static int checkMarker(String substr) {
+    int markerLength = substr.length();
+    for (int i=0; i < markerLength-1; i++) {
+      for (int j=i+1; j < markerLength; j++) {
+        if (substr.charAt(i) == substr.charAt(j)) return i;
       }
     }
-    return true;
+    return -1;
   }
   
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
     int markerLength = 14;
+    int counter = 0;
 
     while(in.hasNext()) {
       String stream = in.nextLine();
       if (stream.equals("q!")) break;
 
-      for (int i=0; i<stream.length()-markerLength-1; i++) {
-        boolean isMarker = checkMarker(stream.substring(i,i+markerLength), markerLength);
+      while (counter < stream.length() - markerLength - 1) {
+        int duplicateMarker = checkMarker(
+          stream.substring(counter, counter + markerLength)
+        );
 
-        if (isMarker) {
-          System.out.println(i+markerLength); break;
+        if (duplicateMarker == -1) {
+          System.out.println(counter + markerLength); break;
+        } else {
+          counter += duplicateMarker + 1;
         }
       }
     }
