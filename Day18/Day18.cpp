4db5da96f3886be64bd588061bf0aa42ce04773b
Marcus Ong <marcus.ong.qy@gmail.com>
2022-12-18 17:50:58 +0800

    day 18 part 1 complete

---
+++ Day18/Day18.cpp
@@ -0,0 +1,56 @@
+// Day 18
+// https://adventofcode.com/2022/day/18
+
+#include <iostream>
+#include <bits/stdc++.h>
+
+#define WORLD_SIZE 60
+
+using namespace std;
+
+int placeBlockAndGetAdjacency(bool (&world)[WORLD_SIZE][WORLD_SIZE][WORLD_SIZE], vector<int> block) {
+    int adj = 0;
+    
+    for (int dx : {-1, 1}) {
+        if (world[block[0]+dx][block[1]][block[2]]) adj++;
+    }
+    
+    for (int dy : {-1, 1}) {
+        if (world[block[0]][block[1]+dy][block[2]]) adj++;
+    }
+
+    for (int dz : {-1, 1}) {
+        if (world[block[0]][block[1]][block[2]+dz]) adj++;
+    }
+
+    world[block[0]][block[1]][block[2]] = true;
+
+    return -(adj-3)*2;
+}
+
+int main() {
+    bool world[WORLD_SIZE][WORLD_SIZE][WORLD_SIZE] = {{{0}}};
+    string rgxstr = "(\\d+),(\\d+),(\\d+)";
+    int totalArea = 0;
+    std::regex rgx = std::regex(rgxstr);
+    std::smatch sm;
+    
+    while (true) {
+        string line;
+        std::cin >> line;
+        if (line == "q!") break;
+
+        std::regex_search(line, sm, rgx);
+
+        int x=stoi(sm[1]);
+        int y=stoi(sm[2]);
+        int z=stoi(sm[3]);
+
+        int darea = placeBlockAndGetAdjacency(world, {x+1, y+1, z+1}); //zero offset
+        totalArea += darea;
+    }
+
+    std::cout << "Total area: " << totalArea << "\n";
+    
+    return 0;
+}
